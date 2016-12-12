2947187cc2065bd50508bb806ab55c198c6c9d5f
Nick <nickmead@live.com.au>
2016-12-12 22:38:27 +1000

    shield logic and style

--- public/app.js
+++ public/app.js
@@ -1,137 +1,183 @@
-var app = angular.module('mainApp', ['ngRoute']);
+var app = angular.module("mainApp", ["ngRoute", "ngStorage"]);
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: '/index.html'
-  });
+app.config(["$routeProvider", function($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl: "/index.html"
+	});
 }]);
 
-app.factory('socket', function ($rootScope) {
-  var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
+app.factory("socket", function ($rootScope) {
+	var socket = io.connect();
+	return {
+		on: function (eventName, callback) {
+			socket.on(eventName, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function (eventName, data, callback) {
+			socket.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(socket, args);
+					}
+				});
+			});
+		}
+	};
 });
 
-app.controller('mainCtrl', function($scope, $timeout, socket) {
+app.controller("mainCtrl", function($scope, $timeout, $localStorage, socket) {
 	$scope.msg = "";
 	$scope.me = {};
-  $scope.moveDirection = {};
+	$scope.moveDirection = {};
 	$scope.players = {};
+	var MAXSQUARES = 10;
 
-  var arrowTimeout = 100;
+	var arrowTimeout = 100;
 
-	$scope.newplayer = function() {
-		socket.emit('newplayer');
+	loadUser();
+
+	function loadUser() {
+		var user = {};
+		user.userid = $localStorage.userid;
+		if (user.userid) {
+			return login(user);
+		} else {
+			$scope.me.userid = "";
+		}
 	}
 
-	socket.on('newplayer', function(player) {
+	$scope.register = function() {
+		socket.emit("register");
+	};
+
+	socket.on("register", function(player) {
 		$scope.me = player;
+		if (player.userid) $localStorage.userid = player.userid;
 	});
 
-  $scope.keypress = function(event) {
-    var keycode = event.keyCode;
-    // left arrow 37
-    switch(keycode) {
-      case 37:
-        // alert("left");
-        $scope.moveDirection.col = -1;
-        $timeout(arrowMove, arrowTimeout);
-        break;
-      case 38:
-        // alert("up");
-        $scope.moveDirection.row = -1;
-        $timeout(arrowMove, arrowTimeout);
-        break;
-      case 39:
-        // alert("right");
-        $scope.moveDirection.col = 1;
-        $timeout(arrowMove, arrowTimeout);
-        break;
-      case 40:
-        // alert("down");
-        $scope.moveDirection.row = 1;
-        $timeout(arrowMove, arrowTimeout);
-        break;
-      default:
-        // do nothing
-    }
-  }
+	$scope.login = function(user) {
+		login(user);
+	};
 
-  function arrowMove() {
-    var row = $scope.moveDirection.row || 0;
-    var col = $scope.moveDirection.col || 0;
-    if (!row && !col) return;
-    // alert(row + "," + col);
-    $scope.move(row, col);
-    $scope.moveDirection = {};
-  }
-
-	$scope.newgame = function(userid) {
-		socket.emit('newgame', userid);
+	function login(user) {
+		socket.emit("login", user);
 	}
 
-	$scope.getSquare = function(row,col) {
-    var square = {};
-		var x = $scope.me.x + col;
-		var y = $scope.me.y + row;
-    if (x <= 0 || y <= 0 || x > 10 || y > 10) {
-      square.class = "wall";
-      square.content = "wall";
-      return square;
-    }
+	$scope.logout = function() {
+		$localStorage.userid = null;
+	};
+
+	$scope.keypress = function(event) {
+		var keycode = event.keyCode;
+    // left arrow 37
+		switch(keycode) {
+		case 37:
+        // alert("left");
+			$scope.moveDirection.col = -1;
+			$timeout(arrowMove, arrowTimeout);
+			break;
+		case 38:
+        // alert("up");
+			$scope.moveDirection.row = -1;
+			$timeout(arrowMove, arrowTimeout);
+			break;
+		case 39:
+        // alert("right");
+			$scope.moveDirection.col = 1;
+			$timeout(arrowMove, arrowTimeout);
+			break;
+		case 40:
+        // alert("down");
+			$scope.moveDirection.row = 1;
+			$timeout(arrowMove, arrowTimeout);
+			break;
+		default:
+        // do nothing
+		}
+	};
+
+	function arrowMove() {
+		var row = $scope.moveDirection.row || 0;
+		var col = $scope.moveDirection.col || 0;
+		if (!row && !col) return;
+    // alert(row + "," + col);
+		$scope.move(row, col);
+		$scope.moveDirection = {};
+	}
+
+	$scope.respawn = function(userid) {
+		socket.emit("respawn", userid);
+	};
+
+	$scope.getSquare = function(playerRow,playerCol) {
+		var square = {};
+
+		var x = $scope.me.x + playerCol;
+		var y = $scope.me.y + playerRow;
+
+		if (x <= 0) x = MAXSQUARES;
+		if (x > MAXSQUARES) x = 1;
+		if (y <= 0) y = MAXSQUARES;
+		if (y > MAXSQUARES) y = 1;
+
 		for (var player in $scope.players) {
 			var thisPlayer = $scope.players[player];
 			if (thisPlayer.level == $scope.me.level && thisPlayer.x == x && thisPlayer.y == y) {
-        square.content = thisPlayer.userid;
-        square.class = (thisPlayer.userid == $scope.me.userid) ? "me" : "enemy";
-        return square;
-      }
+				square.isPlayer = true;
+				square.content = thisPlayer.score;
+				square.class = (thisPlayer.userid == $scope.me.userid) ? "me" : "enemy";
+				return square;
+			}
 		}
-    square.content = x + "," + y;
-    square.class = "empty";
-    return square;
-	}
+		square.content = x + "," + y;
+		square.class = "empty";
+		return square;
+	};
+
+	$scope.getShield = function(playerRow, playerCol, shieldRow, shieldCol) {
+		if (shieldRow == 0 && shieldCol == 0) return "shield";
+
+		var x = $scope.me.x + playerCol;
+		var y = $scope.me.y + playerRow;
+
+		if (x <= 0) x = MAXSQUARES;
+		if (x > MAXSQUARES) x = 1;
+		if (y <= 0) y = MAXSQUARES;
+		if (y > MAXSQUARES) y = 1;
+
+		for (var i in $scope.players) {
+			var thisPlayer = $scope.players[i];
+			if (thisPlayer.level == $scope.me.level && thisPlayer.x == x && thisPlayer.y == y) {
+				return thisPlayer.shields[shieldRow + "," + shieldCol] ? "shield red" : "shield green";
+			}
+		}
+		return "shield";
+	};
 
 	$scope.move = function(row,col) {
-    var x = $scope.me.x + col;
-    var y = $scope.me.y + row;
-    if (!x || !y || x > 10 || y > 10) return;
-    if (!row && !col) return;
+		if (!row && !col) return;
 		var move = {
 			userid: $scope.me.userid,
 			row: row,
 			col: col
 		};
-		socket.emit('move', move);
-	}
+		socket.emit("move", move);
+	};
 
-	socket.on('update', function(player) {
+	socket.on("update", function(player) {
 		$scope.players[player.userid] = player;
 		if (player.userid == $scope.me.userid) {
 			$scope.me = player;
-      if (!player.alive) alert("you died");
+			if (!player.alive) alert("you died");
 		}
 	});
 
-	socket.on('msg', function(msg) {
+	socket.on("msg", function(msg) {
 		$scope.msg = msg;
 	});
 
--- server.js
+++ server.js
@@ -1,58 +1,89 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var shortid = require('shortid');
-app.use(express.static(__dirname + '/public'));
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var shortid = require("shortid");
 
 var MAXSQUARES = 10;
 var players = {};
 
-io.on('connection', function(socket) {
-	console.log('new connection');
+app.use(express.static(__dirname + "/public"));
+server.listen(8000, function() {
+	console.log("server up and running at 8000 port");
+});
 
-	socket.on('newplayer', function() {
+io.on("connection", function(socket) {
+	console.log("new connection");
+
+	socket.on("register", function() {
 		var player = {
 			userid: shortid.generate(),
 			level: 1,
-			score: 5,
+			score: 1,
 			x: null,
 			y: null,
+			shields: {
+				"-1,-1": true,
+				"-1,0": true,
+				"-1,1": true,
+				"0,-1": true,
+				"0,1": true,
+				"1,-1": true,
+				"1,0": true,
+				"1,1": true
+			},
 			alive: false
 		};
+		player = removeShield(player);
 		players[player.userid] = player;
-		console.log(players);
-		socket.emit('newplayer', player);
+		// console.log(players);
+		socket.userid = player.userid;
+		socket.emit("register", player);
 	});
 
-	socket.on('newgame', function(userid) {
-		var player = getPlayer(userid);
-		if (!player) return socket.emit('newplayer', {});
-		player = newgame(player);
-		if (player.alive) {
-			players[player.userid] = player;
-			io.sockets.emit('update', player);
-			for (var player in players) {
-				socket.emit('update', players[player]);
-			}
-		} else {
-			socket.emit('msg', 'no game found');
+	socket.on("disconnect", function() {
+		var userid = socket.userid;
+		if (userid){
+			console.log("disconnection " + userid);
+			players[userid].alive = false;
 		}
 	});
 
-	socket.on('move', function(move) {
+	socket.on("login", function(player) {
+		if (!player.userid) return;
+		for (var i in players) {
+			var thisPlayer = players[i];
+			if (thisPlayer.userid == player.userid) {
+				socket.userid = player.userid;
+				return socket.emit("register", thisPlayer);
+			}
+		}
+	});
+
+	socket.on("respawn", function(userid) {
+		var player = getPlayer(userid);
+		if (!player) return socket.emit("register", {});
+		player = respawn(player);
+		if (player.alive) {
+			players[player.userid] = player;
+			io.sockets.emit("update", player);
+			for (var i in players) {
+				socket.emit("update", players[i]);
+			}
+		} else {
+			socket.emit("msg", "no game found");
+		}
+	});
+
+	socket.on("move", function(move) {
 		var updates = movePlayer(move);
 		for (var i in updates){
 			var player = updates[i];
 			players[player.userid] = player;
-			io.sockets.emit('update', player);
+			io.sockets.emit("update", player);
 		}
 	});
 
-});
-
-server.listen(8000, function() {
-	console.log('server up and running at 8000 port');
 });
 
 function getPlayer(userid) {
@@ -61,20 +92,31 @@ function getPlayer(userid) {
 
 function movePlayer(move){
 	var updates = [];
+	var shieldToCheck = (-1*move.row) + "," + (-1*move.col);
 	var mover = getPlayer(move.userid);
+
 	mover.x += move.col;
 	mover.y += move.row;
 
+	if (mover.x <= 0) mover.x = MAXSQUARES;
+	if (mover.x > MAXSQUARES) mover.x = 1;
+	if (mover.y <= 0) mover.y = MAXSQUARES;
+	if (mover.y > MAXSQUARES) mover.y = 1;
+
 	for (var player in players){
-		var thisPlayer = players[player];
-		if (thisPlayer.userid == move.userid) continue;
-		if (thisPlayer.level == mover.level && thisPlayer.x == mover.x && thisPlayer.y == mover.y) {
-			thisPlayer.score--;
-			thisPlayer.alive = false;
-			thisPlayer.x = null;
-			thisPlayer.y = null;
-			mover.score++;
-			updates.push(thisPlayer);
+		var enemy = players[player];
+		if (enemy.userid == move.userid) continue;
+		if (enemy.level == mover.level && enemy.x == mover.x && enemy.y == mover.y) {
+			if (enemy.shields[shieldToCheck]) {
+				// mover hit enemy shield
+				mover = loseLife(mover);
+				enemy = gainLife(enemy);
+			} else {
+				// mover missed enemy shield
+				mover = gainLife(mover);
+				enemy = loseLife(enemy);
+			}
+			updates.push(enemy);
 			break;
 		}
 	}
@@ -84,24 +126,65 @@ function movePlayer(move){
 
 }
 
-function newgame(newplayer) {
+function removeShield(player) {
+	var count = 0;
+	var randomShield;
+	for (var i in player.shields) {
+		if (!player.shields[i]) continue;
+		if (Math.random() < 1/++count) randomShield = i;
+	}
+	player.shields[randomShield] = false;
+	return player;
+}
+
+function addShield(player) {
+	var count = 0;
+	var randomShield;
+	for (var i in player.shields) {
+		if (player.shields[i]) continue;
+		if (Math.random() < 1/++count) randomShield = i;
+	}
+	player.shields[randomShield] = true;
+	return player;
+}
+
+function loseLife(player) {
+	if (player.score > 1) {
+		player.score--;
+		player = addShield(player);
+	}
+	player.alive = false;
+	player.x = null;
+	player.y = null;
+	return player;
+}
+
+function gainLife(player) {
+	if (player.score < 8) {
+		player.score++;
+		player = removeShield(player);
+	}
+	return player;
+}
+
+function respawn(player) {
 	for (var x = 1; x <= MAXSQUARES; x++) {
 		for (var y = 1; y <= MAXSQUARES; y++) {
 			var allowed = true;
-			for (var player in players) {
-				var thisPlayer = players[player];
-				if (thisPlayer.level == newplayer.level && thisPlayer.x == x && thisPlayer.y == y) {
+			for (var i in players) {
+				var thisPlayer = players[i];
+				if (thisPlayer.level == player.level && thisPlayer.x == x && thisPlayer.y == y) {
 					allowed = false;
 					break;
 				}
 			}
 			if (allowed) {
-				newplayer.x = x;
-				newplayer.y = y;
-				newplayer.alive = true;
-				return newplayer;
+				player.x = x;
+				player.y = y;
+				player.alive = true;
+				return player;
 			}
 		}
 	}
-	return newplayer;
+	return player;
 }
